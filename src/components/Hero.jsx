740b2357c12d01341a01221b4e5c0bbbd7aa3272
Patrick Mahloy <patrick.mahloy@gmail.com>
2022-06-19 15:51:21 -0400

    made Hero card

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,47 +5,36 @@ import './Hero.css';
 const Hero = () => {
 	return (
 		<div className="Hero container text-white col-xxl-8 px-4">
-			<div className="row flex-lg-row-reverse align-items-center g-5 py-5">
-				<div className="col-10 col-sm-8 col-lg-6">
-					{/* <img
-						src={profile}
-						className="d-block mx-lg-auto w-75 bg-white"
-						alt="..."
-						loading="lazy"
-					/> */}
-				</div>
-				<div className="col-lg-6">
-					<h1 className="display-5 fw-bold lh-1 mb-3">
-						Hi, I'm Patrick
-					</h1>
-					<p className="lead">
-						I'm a nerd based out of Richmond, VA. I love building apps and
-						websites for people to use. Let's build something!
-					</p>
-					<div className="d-grid gap-2 d-md-flex justify-content-md-start">
-						<button
-							type="button"
-							className="btn btn-outline-light btn-lg px-4 me-md-2"
-						>
-							Resume
-						</button>
-						<button
-							type="button"
-							className="btn btn-outline-light btn-lg px-4 me-md-2"
-						>
-							Projects
-						</button>
-						<button
-							type="button"
-							className="btn btn-outline-light btn-lg px-4 me-md-2"
-						>
-							Contact
-						</button>
-					</div>
+			<div className="hero-card">
+				<h1 className="display-5 fw-bold lh-1 mb-3">Hi, I'm Patrick</h1>
+				<p className="lead">
+					I'm a nerd based out of Richmond, VA.
+					<br />I love building apps and websites for people to use. Let's build
+					something!
+				</p>
+				<div className="d-grid gap-2 d-md-flex justify-content-md-start">
+					<button
+						type="button"
+						className="btn btn-outline-light btn-lg px-4 me-md-2"
+					>
+						Resume
+					</button>
+					<button
+						type="button"
+						className="btn btn-outline-light btn-lg px-4 me-md-2"
+					>
+						Projects
+					</button>
+					<button
+						type="button"
+						className="btn btn-outline-light btn-lg px-4 me-md-2"
+					>
+						Contact
+					</button>
 				</div>
 			</div>
 		</div>
 	);
-}
+};
 
 export default Hero;

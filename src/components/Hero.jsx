852142313a678359f023c0f4e8ec38d4bcc385c3
Patrick Mahloy <patrick.mahloy@gmail.com>
2022-06-19 14:57:53 -0400

    removed profile image, refactored Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,15 +5,15 @@ import profile from '../assets/profile.png';
 
 const Hero = () => {
 	return (
-		<div className="Hero container text-secondary col-xxl-8 px-4 py-5">
+		<div className="Hero container text-secondary col-xxl-8 px-4">
 			<div className="row flex-lg-row-reverse align-items-center g-5 py-5">
 				<div className="col-10 col-sm-8 col-lg-6">
-					<img
+					{/* <img
 						src={profile}
-						className="d-block mx-lg-auto w-75 rounded bg-secondary"
+						className="d-block mx-lg-auto w-75 bg-white"
 						alt="..."
 						loading="lazy"
-					/>
+					/> */}
 				</div>
 				<div className="col-lg-6">
 					<h1 className="display-5 text-white fw-bold lh-1 mb-3">
@@ -26,19 +26,19 @@ const Hero = () => {
 					<div className="d-grid gap-2 d-md-flex justify-content-md-start">
 						<button
 							type="button"
-							className="btn btn-primary btn-lg px-4 me-md-2"
+							className="btn btn-outline-light btn-lg px-4 me-md-2"
 						>
 							Resume
 						</button>
 						<button
 							type="button"
-							className="btn btn-primary btn-lg px-4 me-md-2"
+							className="btn btn-outline-light btn-lg px-4 me-md-2"
 						>
 							Projects
 						</button>
 						<button
 							type="button"
-							className="btn btn-primary btn-lg px-4 me-md-2"
+							className="btn btn-outline-light btn-lg px-4 me-md-2"
 						>
 							Contact
 						</button>

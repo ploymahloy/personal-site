9883c132bb45618513be07a7d059daec0ae4b600
Patrick Mahloy <patrick.mahloy@gmail.com>
2022-06-19 14:57:26 -0400

    updated project section

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 
-export default function Card({description, img, title}) {
+import './Card.css';
+
+export default function Card({ description, img, title }) {
 	return (
 		<div className="col mb-4">
 			<div className="card-body bg-white border">
-				<img
-					className="card-img-top"
+        <img
+					className="card-img-top border-bottom border-white"
 					src={img}
 					alt=""
 				/>
-				<div className="card-body p-3">
+				<div className="card-body text-white p-3">
 					<h5 className="card-title">{title}</h5>
-					<p className="card-text">
-						{description}
-					</p>
-					<a href="" className="btn btn-primary w-100 m-1">
+					<p className="card-text">{description}</p>
+					<a href="" className="btn btn-outline-light w-100 m-1">
 						Live
 					</a>
-					<a href="" className="btn btn-primary w-100 m-1">
+					<a href="" className="btn btn-outline-light w-100 m-1">
 						Github
 					</a>
 				</div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
 import Card from './Card';
+import './Projects.css';
 
 import basiclopedia from '../assets/basiclopedia.png';
 import cli_calc from '../assets/cli_calc.png';
+import client_directory from '../assets/client_directory.png';
 import fbc from '../assets/fbc.png';
+import mortgage_calc from '../assets/mortgage_calc.png';
 import note_taker from '../assets/note_taker.png';
+
+const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
 
 const Projects = () => {
 	return (
@@ -13,24 +18,44 @@ const Projects = () => {
 			<h1 className="display-5 text-white fw-bold lh-1 mb-3">Projects</h1>
 			<div className="row row-cols-1 row-cols-md-2 row-cols-lg-3">
 				<Card
-					img={fbc}
-					title="Construction Work Orders"
-					description="Internal software for construction companies to track work orders. Powered by React and Bootstrap."
+          img={fbc}
+          imgClass={false}
+          title="Construction Work Orders"
+          description={lorem}
+					// description="Internal software for construction companies to track work orders. Powered by React and Bootstrap."
 				/>
 				<Card
-					img={basiclopedia}
-					title="Dictionary"
-					description="React Typescript project populated with Free Dictionary API data queried based on user input."
+          img={basiclopedia}
+          imgClass={false}
+          title="Dictionary"
+          description={lorem}
+					// description="React Typescript project populated with Free Dictionary API data queried based on user input."
 				/>
 				<Card
-					img={cli_calc}
-					title="CLI Calculator"
-					description="CLI calculator built with readlineSync in Typescript."
+          img={cli_calc}
+          imgClass={false}
+          title="CLI Calculator"
+          description={lorem}
+					// description="CLI calculator built with readlineSync in Typescript."
 				/>
 				<Card
-					img={note_taker}
-					title="Note Taker"
-					description="A CRUD app that can be used to write, save, and delete notes. This app uses a NodeJS/Express backend and save and retrieve note data."
+          img={note_taker}
+          imgClass={false}
+          title="Note Taker"
+          description={lorem}
+					// description="A CRUD app that can be used to write, save, and delete notes. This app uses a NodeJS/Express backend and save and retrieve note data."
+				/>
+				<Card
+          img={client_directory}
+          imgClass={false}
+          title="Client Directory"
+          description={lorem}
+				/>
+        <Card
+          img={mortgage_calc}
+          imgClass={true}
+          title="Mortgage Calculator"
+          description={lorem}
 				/>
 			</div>
 		</div>

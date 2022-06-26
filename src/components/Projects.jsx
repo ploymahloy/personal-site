3a47de4b992633dfbd15f950d263c03b967b037f
Patrick Mahloy <patrick.mahloy@gmail.com>
2022-06-25 21:14:15 -0400

    minor tweaks in spacing

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,11 +6,29 @@ import './Projects.css';
 const Projects = () => {
 	return (
 		<div className="projects-container" id="Projects">
-			<h2 className="projects-card-title">Projects</h2>
-				<Card text="Work Order Portal" color="rgb(194, 174, 158)" link="https://github.com/ploymahloy/construction-work-orders"/>
-				<Card text="Dictionary App" color="rgb(220, 135, 73)" link="https://github.com/ploymahloy/dictionaryapi"/>
-				<Card text="CLI Calculator" color="rgb(243, 170, 72)" link="https://github.com/ploymahloy/cli-calculator"/>
-				<Card text="Budget Tracker" color="rgb(151, 165, 163)" link="https://github.com/ploymahloy/budget-app"/>
+			<div className="projects-card">
+				<h2 className="projects-card-title">Projects</h2>
+				<Card
+					text="Work Order Portal"
+					color="rgb(194, 174, 158)"
+					link="https://github.com/ploymahloy/construction-work-orders"
+				/>
+				<Card
+					text="Dictionary App"
+					color="rgb(220, 135, 73)"
+					link="https://github.com/ploymahloy/dictionaryapi"
+				/>
+				<Card
+					text="CLI Calculator"
+					color="rgb(243, 170, 72)"
+					link="https://github.com/ploymahloy/cli-calculator"
+				/>
+				<Card
+					text="Budget Tracker"
+					color="rgb(151, 165, 163)"
+					link="https://github.com/ploymahloy/budget-app"
+				/>
+			</div>
 		</div>
 	);
 };

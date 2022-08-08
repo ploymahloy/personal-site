e278bd70025ee6c0da63653bdbfffcb6005f7be9
Patrick Mahloy <patrick.mahloy@gmail.com>
2022-08-07 23:35:30 -0400

    added sidenav menu for small screens

--- layouts/Default.js
+++ layouts/Default.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { TiThMenu } from 'react-icons/ti';
+import React, { useState } from 'react';
+import { SiGithub, SiLinkedin } from 'react-icons/si';
+import { TiThMenu, TiTimes } from 'react-icons/ti';
 
 import styles from '../styles/Default.module.scss';
 
 const Default = ({ children }) => {
+	const [isActive, setIsActive] = useState(false);
+
 	return (
 		<>
 			<ul className={styles.menu}>
@@ -15,9 +18,40 @@ const Default = ({ children }) => {
 					</li>
 				</span>
 
-				<a href="#" className={styles.menu_icon} >
+				<a
+					href="#!"
+					className={styles.menu_icon}
+					onClick={() => setIsActive(true)}
+				>
 					<TiThMenu />
 				</a>
+				<div
+					className={styles.sidenav}
+					style={{ display: isActive ? 'block' : 'none' }}
+				>
+					<ul className={styles.sidenav_menu}>
+						<TiTimes
+							className={styles.sidenav_menu_x}
+							onClick={() => setIsActive(false)}
+						/>
+						<li>
+							<a href="/projects" className={styles.sidenav_menu_link}>
+								Projects
+							</a>
+						</li>
+						<li>
+							<a href="#" className={styles.sidenav_menu_link}>
+								Resume
+							</a>
+						</li>
+						<li>
+							<a href="/contact" className={styles.sidenav_menu_link}>
+								Contact
+							</a>
+						</li>
+					</ul>
+				</div>
+
 				<span className={styles.right}>
 					<li className={styles.menu_underlay}>
 						<a href="/projects" className={styles.link}>
@@ -39,7 +73,25 @@ const Default = ({ children }) => {
 					</li>
 				</span>
 			</ul>
-			{children}
+			<div className={styles.page_content}>{children}</div>
+			<footer className={styles.footer}>
+				<div className={styles.footer_icons}>
+					<a
+						href="https://github.com/ploymahloy"
+						target="_blank"
+						className={styles.footer_icon}
+					>
+						<SiGithub />
+					</a>
+					<a
+						href="https://www.linkedin.com/in/patrickmahloy/"
+						target="_blank"
+						className={styles.footer_icon}
+					>
+						<SiLinkedin />
+					</a>
+				</div>
+			</footer>
 		</>
 	);
 };
